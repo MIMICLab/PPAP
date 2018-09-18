5a34595f362b07816ac3ff3bcad5f20c17a1619b
tgisaturday <jamesk1228@gmail.com>
2018-09-18 10:25:58 +0900

    create deprecated

---
+++ deprecated/model.py
@@ -0,0 +1,395 @@
+import tensorflow as tf
+import numpy as np
+
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+def he_normal_init(size):
+    in_dim = size[0]
+    he_stddev = tf.sqrt(2./in_dim)
+    return tf.random_normal(shape=size, stddev=he_stddev)
+
+def epsilon_init(initial, size):
+    in_dim = size[0]
+    stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.add(initial,tf.random_normal(shape=size, stddev=stddev))
+
+def delta_init(initial, size):
+    in_dim = size[0]
+    stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.add(initial,tf.random_normal(shape=size, stddev=stddev))
+
+def ppap_autoencoder(input_shape, n_filters, filter_sizes, z_dim, x,var_A, var_G):
+    current_input = x    
+    encoder = []
+    shapes_enc = []
+    with tf.name_scope("DP_Encoder"):
+        for layer_i, n_output in enumerate(n_filters[1:]):
+            n_input = current_input.get_shape().as_list()[3]
+            shapes_enc.append(current_input.get_shape().as_list())
+            W = tf.Variable(he_normal_init([filter_sizes[layer_i],filter_sizes[layer_i],n_input, n_output]))
+            var_A.append(W)
+            encoder.append(W)
+            conv = tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME')          
+            conv = tf.contrib.layers.batch_norm(conv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+            output = tf.nn.leaky_relu(conv)
+            current_input = output
+        encoder.reverse()
+        shapes_enc.reverse() 
+        z_flat = tf.layers.flatten(current_input)
+        z_flat_dim = int(z_flat.get_shape()[1])
+        W_fc1 = tf.Variable(xavier_init([z_flat_dim, z_dim]))
+        var_A.append(W_fc1)
+        z = tf.matmul(z_flat,W_fc1)
+        z = tf.contrib.layers.batch_norm(z,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        
+    with tf.name_scope("DP_Encoder_Reverse"): 
+        z_auto = tf.matmul(z,tf.transpose(W_fc1)) 
+        z_auto = tf.contrib.layers.batch_norm(z_auto,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_auto = tf.nn.relu(z_auto)
+        current_input = tf.reshape(z_auto, [-1, 4, 4, n_filters[-1]])          
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            deconv = tf.nn.conv2d_transpose(current_input, W_enc,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')
+
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        a = current_input
+        
+    with tf.name_scope("DP_decoder"):        
+        W_fc2 = tf.Variable(xavier_init([z_dim, z_flat_dim]))
+        var_G.append(W_fc2)
+        z_ = tf.matmul(z,W_fc2) 
+        z_ = tf.contrib.layers.batch_norm(z_,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_ = tf.nn.relu(z_)
+        current_input = tf.reshape(z_, [-1, 4, 4, n_filters[-1]])           
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            W = tf.Variable(he_normal_init(W_enc.get_shape().as_list()))
+            var_G.append(W)
+            deconv = tf.nn.conv2d_transpose(current_input, W,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')       
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        g = current_input
+  
+    return g, a
+
+def edp_autoencoder(input_shape, n_filters, filter_sizes,z_dim, x, Y, var_A, var_G, init_e):
+    current_input = x    
+    encoder = []
+    shapes_enc = []
+    with tf.name_scope("DP_Encoder"):
+        for layer_i, n_output in enumerate(n_filters[1:]):
+            n_input = current_input.get_shape().as_list()[3]
+            shapes_enc.append(current_input.get_shape().as_list())
+            W = tf.Variable(he_normal_init([filter_sizes[layer_i],filter_sizes[layer_i],n_input, n_output]))
+            var_A.append(W)
+            encoder.append(W)
+            conv = tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME')          
+            conv = tf.contrib.layers.batch_norm(conv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+            output = tf.nn.leaky_relu(conv)
+            current_input = output
+        encoder.reverse()
+        shapes_enc.reverse() 
+        z_flat = tf.layers.flatten(current_input)
+        z_flat_dim = int(z_flat.get_shape()[1])
+        W_fc1 = tf.Variable(xavier_init([z_flat_dim, z_dim]))
+        var_A.append(W_fc1)
+        z = tf.matmul(z_flat,W_fc1)
+        z = tf.contrib.layers.batch_norm(z,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        
+    with tf.name_scope("DP_Encoder_Reverse"): 
+        z_auto = tf.matmul(z,tf.transpose(W_fc1)) 
+        z_auto = tf.contrib.layers.batch_norm(z_auto,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_auto = tf.nn.relu(z_auto)
+        current_input = tf.reshape(z_auto, [-1, 4, 4, n_filters[-1]])          
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            deconv = tf.nn.conv2d_transpose(current_input, W_enc,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:     
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        a = current_input
+        
+    with tf.name_scope("Noise_Applier"):
+        z = tf.nn.tanh(z)        
+        z_original = z
+        W_epsilon = tf.Variable(epsilon_init(init_e, [z_dim]))
+        var_G.append(W_epsilon)        
+        W_epsilon = tf.maximum(tf.abs(W_epsilon),1e-8)
+        dp_lambda = tf.divide(2.0,W_epsilon)
+        W_noise = tf.multiply(Y,dp_lambda)
+        z = tf.add(z,W_noise)
+        z_noise_applied = z
+        
+    with tf.name_scope("DP_decoder"):        
+        W_fc2 = tf.Variable(xavier_init([z_dim, z_flat_dim]))
+        var_G.append(W_fc2)
+        z_ = tf.matmul(z,W_fc2) 
+        z_ = tf.contrib.layers.batch_norm(z_,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_ = tf.nn.relu(z_)
+        current_input = tf.reshape(z_, [-1, 4, 4, n_filters[-1]])           
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            W = tf.Variable(he_normal_init(W_enc.get_shape().as_list()))
+            var_G.append(W)
+            deconv = tf.nn.conv2d_transpose(current_input, W,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')           
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        g = current_input
+  
+    return g, a, z_original, z_noise_applied, W_epsilon, W_noise
+
+def eddp_autoencoder(input_shape, n_filters, filter_sizes, z_dim, x, Y, var_A, var_G,init_e, init_d):
+    current_input = x    
+    encoder = []
+    shapes_enc = []
+    with tf.name_scope("DP_Encoder"):
+        for layer_i, n_output in enumerate(n_filters[1:]):
+            n_input = current_input.get_shape().as_list()[3]
+            shapes_enc.append(current_input.get_shape().as_list())
+            W = tf.Variable(he_normal_init([filter_sizes[layer_i],filter_sizes[layer_i],n_input, n_output]))
+            var_A.append(W)
+            encoder.append(W)
+            conv = tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME')          
+            conv = tf.contrib.layers.batch_norm(conv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+            output = tf.nn.leaky_relu(conv)
+            current_input = output
+        encoder.reverse()
+        shapes_enc.reverse() 
+        z_flat = tf.layers.flatten(current_input)
+        z_flat_dim = int(z_flat.get_shape()[1])
+        W_fc1 = tf.Variable(xavier_init([z_flat_dim, z_dim]))
+        var_A.append(W_fc1)
+        z = tf.matmul(z_flat,W_fc1)
+        z = tf.contrib.layers.batch_norm(z,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+
+    with tf.name_scope("DP_Encoder_Reverse"): 
+        z_auto = tf.matmul(z,tf.transpose(W_fc1)) 
+        z_auto = tf.contrib.layers.batch_norm(z_auto,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_auto = tf.nn.relu(z_auto)
+        current_input = tf.reshape(z_auto, [-1, 4, 4, n_filters[-1]])          
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            deconv = tf.nn.conv2d_transpose(current_input, W_enc,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        a = current_input        
+    with tf.name_scope("Noise_Applier"):
+        z = tf.nn.tanh(z)        
+        z_original = z
+        W_epsilon = tf.Variable(epsilon_init(init_e, [z_dim]))
+        var_G.append(W_epsilon)
+        W_epsilon = tf.maximum(tf.abs(W_epsilon),1e-8)       
+        W_delta = tf.Variable(delta_init(init_d, [z_dim]))
+        var_G.append(W_delta)
+        W_delta = tf.maximum(tf.abs(W_delta),1e-8)
+        dp_delta = tf.log(tf.divide(1.25,W_delta))
+        dp_delta = tf.maximum(dp_delta,0)
+        dp_delta = tf.sqrt(tf.multiply(2.0,dp_delta))      
+        dp_lambda = tf.multiply(dp_delta,tf.divide(2.0,W_epsilon))
+        W_noise = tf.multiply(Y,dp_lambda)
+        z = tf.add(z,W_noise)
+        z_noise_applied = z
+    with tf.name_scope("DP_decoder"):        
+        W_fc2 = tf.Variable(xavier_init([z_dim, z_flat_dim]))
+        var_G.append(W_fc2)
+        z_ = tf.matmul(z,W_fc2) 
+        z_ = tf.contrib.layers.batch_norm(z_,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_ = tf.nn.relu(z_)
+        current_input = tf.reshape(z_, [-1, 4, 4, n_filters[-1]])           
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            W = tf.Variable(he_normal_init(W_enc.get_shape().as_list()))
+            var_G.append(W)
+            deconv = tf.nn.conv2d_transpose(current_input, W,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')           
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.relu(deconv)
+            current_input = output
+        g = current_input
+ 
+    return g, a, z_original, z_noise_applied, W_epsilon, W_delta, W_noise
+
+def hacker(input_shape, n_filters, filter_sizes,z_dim, x, var_G, reuse=False):
+    current_input = x    
+    encoder = []
+    shapes_enc = []
+    idx = 0
+    with tf.name_scope("Encoder"):
+        for layer_i, n_output in enumerate(n_filters[1:]):
+            n_input = current_input.get_shape().as_list()[3]
+            shapes_enc.append(current_input.get_shape().as_list())
+            if reuse ==False:
+                W = tf.Variable(he_normal_init([filter_sizes[layer_i],filter_sizes[layer_i],n_input, n_output]))
+                var_G.append(W)
+            else:
+                W = var_G[idx]
+                idx+=1
+            encoder.append(W)
+            conv = tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME')          
+            conv = tf.contrib.layers.batch_norm(conv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+            output = tf.nn.leaky_relu(conv)
+            current_input = output
+        encoder.reverse()
+        shapes_enc.reverse()    
+        z_flat = tf.layers.flatten(current_input)
+        z_flat_dim = int(z_flat.get_shape()[1])
+        if reuse == False:
+            W_fc1 = tf.Variable(xavier_init([z_flat_dim, z_dim]))
+            var_G.append(W_fc1)
+        else:
+            W_fc1 = var_G[idx]
+            idx+=1        
+        z = tf.matmul(z_flat,W_fc1)
+        z = tf.contrib.layers.batch_norm(z,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)        
+    with tf.name_scope("Decoder"): 
+        if reuse == False:
+            W_fc2 = tf.Variable(xavier_init([z_dim, z_flat_dim]))
+            var_G.append(W_fc2)
+        else:
+            W_fc2 = var_G[idx]
+            idx+=1                    
+        z_ = tf.matmul(z,W_fc2) 
+        z_ = tf.contrib.layers.batch_norm(z_,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_ = tf.nn.relu(z_)
+        current_input = tf.reshape(z_, [-1, 4, 4, n_filters[-1]])         
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            if reuse == False:
+                W = tf.Variable(he_normal_init(W_enc.get_shape().as_list()))
+                var_G.append(W)
+            else:
+                W = var_G[idx]
+                idx+=1
+            deconv = tf.nn.conv2d_transpose(current_input, W,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')          
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.leaky_relu(deconv)
+            current_input = output  
+        a =  current_input
+    return a
+
+def reverse_generator(input_shape, n_filters, filter_sizes,z_dim, x, z_noise, var_G, reuse=False):
+    current_input = x    
+    encoder = []
+    shapes_enc = []
+    idx = 0
+    with tf.name_scope("Encoder"):
+        for layer_i, n_output in enumerate(n_filters[1:]):
+            n_input = current_input.get_shape().as_list()[3]
+            shapes_enc.append(current_input.get_shape().as_list())
+            if reuse ==False:
+                W = tf.Variable(he_normal_init([filter_sizes[layer_i],filter_sizes[layer_i],n_input, n_output]))
+                var_G.append(W)
+            else:
+                W = var_G[idx]
+                idx+=1
+            encoder.append(W)
+            conv = tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME')          
+            conv = tf.contrib.layers.batch_norm(conv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+            output = tf.nn.leaky_relu(conv)
+            current_input = output
+        encoder.reverse()
+        shapes_enc.reverse()    
+        z_flat = tf.layers.flatten(current_input)
+        z_flat_dim = int(z_flat.get_shape()[1])
+        if reuse == False:
+            W_fc1 = tf.Variable(xavier_init([z_flat_dim, z_dim]))
+            var_G.append(W_fc1)
+        else:
+            W_fc1 = var_G[idx]
+            idx+=1        
+        z = tf.matmul(z_flat,W_fc1)
+        z = tf.contrib.layers.batch_norm(z,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True) 
+        z = tf.subtract(z, z_noise)
+    with tf.name_scope("Decoder"): 
+        if reuse == False:
+            W_fc2 = tf.Variable(xavier_init([z_dim, z_flat_dim]))
+            var_G.append(W_fc2)
+        else:
+            W_fc2 = var_G[idx]
+            idx+=1                    
+        z_ = tf.matmul(z,W_fc2) 
+        z_ = tf.contrib.layers.batch_norm(z_,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)
+        z_ = tf.nn.relu(z_)
+        current_input = tf.reshape(z_, [-1, 4, 4, n_filters[-1]])         
+        for layer_i, shape in enumerate(shapes_enc):
+            W_enc = encoder[layer_i]
+            if reuse == False:
+                W = tf.Variable(he_normal_init(W_enc.get_shape().as_list()))
+                var_G.append(W)
+            else:
+                W = var_G[idx]
+                idx+=1
+            deconv = tf.nn.conv2d_transpose(current_input, W,
+                                     tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
+                                     strides=[1, 2, 2, 1], padding='SAME')           
+            if layer_i == len(n_filters)-2:
+                output = tf.nn.sigmoid(deconv)
+            else:
+                deconv = tf.contrib.layers.batch_norm(deconv,updates_collections=None,decay=0.9, zero_debias_moving_mean=True,is_training=True)           
+                output = tf.nn.leaky_relu(deconv)
+            current_input = output  
+        a =  current_input
+    return a
+
+def discriminator(x,var_D):
+    current_input = x
+    with tf.name_scope("Discriminator"):
+        for i in range(len(var_D)-2):
+            conv = tf.nn.conv2d(current_input, var_D[i], strides=[1,2,2,1],padding='SAME')
+            conv = tf.contrib.layers.layer_norm(conv)
+            current_input = tf.nn.leaky_relu(conv)            
+        h = tf.layers.flatten(current_input)     
+        d = tf.nn.xw_plus_b(h, var_D[-2], var_D[-1])        
+    return d
+
+def gradient_penalty(G_sample, A_true_flat, mb_size, var_D):
+    epsilon = tf.random_uniform(shape=[mb_size, 1, 1, 1], minval=0.,maxval=1.)
+    X_hat = A_true_flat + epsilon * (G_sample - A_true_flat)
+    D_X_hat = discriminator(X_hat,var_D)
+    grad_D_X_hat = tf.gradients(D_X_hat, [X_hat])[0]
+    red_idx = list(range(1, X_hat.shape.ndims))
+    slopes = tf.sqrt(tf.reduce_sum(tf.square(grad_D_X_hat), reduction_indices=red_idx))
+    gradient_penalty = tf.reduce_mean(tf.square(slopes - 1.))
+    return gradient_penalty
---
+++ deprecated/train_epsilon.py
@@ -0,0 +1,151 @@
+import tensorflow as tf
+import numpy as np
+from utils.utils import *
+from model import *
+import sys
+import os
+import math
+import time
+from utils.data_helper import data_loader
+from model import xavier_init, he_normal_init
+
+dataset = sys.argv[1]
+init_epsilon = float(sys.argv[2])
+model_name = sys.argv[3]
+prev_iter = int(sys.argv[4])
+
+mb_size, X_dim, width, height, channels,len_x_train, x_train, len_x_test, x_test  = data_loader(dataset)
+    
+    
+graph = tf.Graph()
+with graph.as_default():
+    session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    sess = tf.Session(config=session_conf)
+
+    with sess.as_default():
+        #input placeholder
+        input_shape=[None, width, height, channels]
+        filter_sizes=[5, 5, 5, 5, 5]        
+        hidden = 128
+        z_dim = 128          
+
+        if dataset == 'celebA' or dataset == 'lsun':        
+            n_filters=[channels, hidden, hidden*2, hidden*4, hidden*8]
+        else:      
+            n_filters=[channels, hidden, hidden*2, hidden*4]
+            
+        X = tf.placeholder(tf.float32, shape=[None, width, height,channels])
+        Z_noise = tf.placeholder(tf.float32, shape=[None,  z_dim])
+        A_true_flat = X
+        
+        #autoencoder variables
+        var_A = []
+        var_G = []
+        var_H = []
+        #discriminator variables
+        W1 = tf.Variable(he_normal_init([5,5,channels, hidden//2]))
+        W2 = tf.Variable(he_normal_init([5,5, hidden//2,hidden]))
+        W3 = tf.Variable(he_normal_init([5,5,hidden,hidden*2]))
+        if dataset == 'celebA' or dataset == 'lsun':
+            W4 = tf.Variable(he_normal_init([5,5,hidden*2,hidden*4]))
+            W5 = tf.Variable(xavier_init([4*4*hidden*4, 1]))
+            b5 = tf.Variable(tf.zeros(shape=[1]))
+            var_D = [W1,W2,W3,W4,W5,b5] 
+        else:
+            W4 = tf.Variable(xavier_init([4*4*hidden*2, 1]))
+            b4 = tf.Variable(tf.zeros(shape=[1]))
+            var_D = [W1,W2,W3,W4,b4] 
+        
+        global_step = tf.Variable(0, name="global_step", trainable=False)        
+
+        G_sample, A_sample, z_original,z_noised, epsilon_layer, z_noise = edp_autoencoder(input_shape, n_filters, filter_sizes,z_dim, A_true_flat, Z_noise, var_A, var_G, init_epsilon)
+        G_hacked = hacker(input_shape, n_filters, filter_sizes,z_dim, G_sample, var_H)
+             
+        D_real_logits = discriminator(A_true_flat, var_D)
+        D_fake_logits = discriminator(G_sample, var_D)
+        
+        gp = gradient_penalty(G_sample, A_true_flat, mb_size,var_D)
+        dp_epsilon = tf.reduce_mean(epsilon_layer)
+        D_loss = tf.reduce_mean(D_fake_logits) - tf.reduce_mean(D_real_logits) +10.0*gp 
+
+        privacy_gain = tf.reduce_mean(tf.pow(A_true_flat - G_hacked,2))    
+        A_loss = tf.reduce_mean(tf.pow(A_true_flat - A_sample,2))  
+        G_loss = -tf.reduce_mean(D_fake_logits) - privacy_gain
+        H_loss = privacy_gain 
+        
+        tf.summary.image('Original',A_true_flat)
+        tf.summary.image('Autoencoded',A_sample)        
+        tf.summary.image('fake',G_sample)
+        tf.summary.image('decoded_from_fake',G_hacked)
+        tf.summary.scalar('D_loss', D_loss)      
+        tf.summary.scalar('G_loss',-tf.reduce_mean(D_fake_logits))
+        tf.summary.scalar('Encoder_loss', A_loss)
+        tf.summary.scalar('privacy_gain',privacy_gain)
+        tf.summary.scalar('epsilon', dp_epsilon)
+        tf.summary.histogram('epsilon_layer',epsilon_layer)
+        tf.summary.histogram('z_original',  z_original) 
+        tf.summary.histogram('z_noise_applied',z_noised) 
+        tf.summary.histogram('z_noise',z_noise) 
+        merged = tf.summary.merge_all()
+
+        num_batches_per_epoch = int((len_x_train-1)/mb_size) + 1
+        A_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(A_loss,var_list=var_A, global_step=global_step)      
+        D_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(D_loss,var_list=var_D, global_step=global_step)
+        G_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_loss,var_list=var_G, global_step=global_step)
+        H_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(H_loss,var_list=var_H, global_step=global_step)
+        
+
+        timestamp = str(int(time.time()))        
+        if not os.path.exists('results/epsilon_DP/'):
+            os.makedirs('results/epsilon_DP/')        
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "results/epsilon_DP/models/{}_".format(dataset) + model_name))
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists('results/epsilon_DP/models/'):
+            os.makedirs('results/epsilon_DP/models/')
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        if not os.path.exists('results/epsilon_DP/dc_out_{}/'.format(dataset)):
+            os.makedirs('results/epsilon_DP/dc_out_{}/'.format(dataset))         
+
+        train_writer = tf.summary.FileWriter('results/graphs/epsilon_DP/{}'.format(dataset),sess.graph)
+        saver = tf.train.Saver(tf.global_variables())
+        sess.run(tf.global_variables_initializer())
+        if prev_iter != 0:
+            saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))  
+        i = prev_iter      
+        for it in range(1000000000):
+            for _ in range(5):
+                if dataset == 'mnist':
+                    X_mb, _ = x_train.train.next_batch(mb_size)
+                    X_mb = np.reshape(X_mb,[-1,28,28,1])
+                elif dataset == 'lsun':
+                    X_mb = x_train.next_batch(mb_size)
+                else:
+                    X_mb = next_batch(mb_size, x_train)
+                    
+                enc_noise = np.random.laplace(0.0,1.0,[mb_size,z_dim]).astype(np.float32)  
+                _, _, D_loss_curr, H_loss_curr = sess.run([D_solver,H_solver, D_loss, H_loss],feed_dict={X: X_mb, Z_noise: enc_noise})     
+            summary, _, _, G_loss_curr, dp_epsilon_curr = sess.run([merged, A_solver,G_solver, G_loss, dp_epsilon],feed_dict={X: X_mb, Z_noise: enc_noise})
+            current_step = tf.train.global_step(sess, global_step)
+            train_writer.add_summary(summary,current_step)
+        
+            if it % 100 == 0:
+                print('Iter: {}; D_loss: {:.4}; G_loss: {:.4}; privacy_gain: {:.4}; epsilon: {:.4};'.format(it,D_loss_curr, G_loss_curr,H_loss_curr, dp_epsilon_curr))
+
+            if it % 1000 == 0:    
+                Xt_mb = x_test[:mb_size]
+                G_sample_curr, A_sample_curr, re_fake_curr = sess.run([G_sample,A_sample, G_hacked], feed_dict={X: Xt_mb, Z_noise: enc_noise})
+                samples_flat = tf.reshape(G_sample_curr,[-1,width,height,channels]).eval()
+                img_set = np.append(Xt_mb[:256], samples_flat[:256], axis=0)
+                samples_flat = tf.reshape(A_sample_curr,[-1,width,height,channels]).eval() 
+                img_set = np.append(img_set, samples_flat[:256], axis=0)                     
+                samples_flat = tf.reshape(re_fake_curr,[-1,width,height,channels]).eval() 
+                img_set = np.append(img_set, samples_flat[:256], axis=0)               
+
+                fig = plot(img_set, width, height, channels)
+                plt.savefig('results/epsilon_DP/dc_out_{}/{}.png'.format(dataset,str(i).zfill(3)), bbox_inches='tight')
+                plt.close(fig)
+                i += 1
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print('Saved model at {} at step {}'.format(path, current_step))
